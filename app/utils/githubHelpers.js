68ef9c8560fc6255b020cd60ba7c2ecaef52fe9e
Karolina Kafel <kr@soundcloud.com>
2016-04-22 20:55:42 +0200

    Fetching github data using axios

---
+++ app/utils/githubHelpers.js
@@ -0,0 +1,26 @@
+var axios = require('axios');
+
+var id = 'YOUR_CLIENT_ID';
+var sec = 'YOU_SECRET_ID';
+var param = '?client_id=' + id + '&client_secret=' + sec;
+
+function getUserInfo(username) {
+    return axios.get('http://api.github.com/users/' + username + param)
+}
+
+var helpers = {
+    getPlayersInfo: function(players) {
+        return axios.all(players.map(function(username){
+           return getUserInfo(username) 
+        })).then(function(info) {
+            return info.map(function(user) {
+                console.info(user.data)
+                return user.data;
+            })
+        }).catch(function(err){
+          console.warn('Error in getPlayersInfo', err)  
+        }) 
+    }
+};
+
+module.exports = helpers;
